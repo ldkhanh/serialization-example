e68e21921aa1e9c1734e1d9fceab73ef149153df
khanhduyle <khanhduyle@assembla.com>
2018-05-31 04:55:48 -0700

    first commit

---
+++ src/main/java/com/khanh/serializationexample/SerializationExampleApplication.java
@@ -0,0 +1,12 @@
+package com.khanh.serializationexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerializationExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerializationExampleApplication.class, args);
+	}
+}
